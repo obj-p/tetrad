0ec9068b600da56b24bf59783ebbd70e5e599b0a
Jason Prasad <jasongprasad@gmail.com>
2024-12-23 12:22:02 -0500

    Add example of grun

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import java.io.ByteArrayInputStream
+import java.util.Scanner
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     antlr
     application
 }
 
+val antlrTasksGroup = "ANTLR"
 val tetradPackage = "com.objp.tetrad"
 
 dependencies {
@@ -18,8 +22,30 @@ application {
 sourceSets {
     main {
         java {
-            srcDirs("build/generated-src/antlr/main")
+            srcDirs("${layout.buildDirectory}/generated-src/antlr/main")
         }
+    }
+}
+
+tasks.register<JavaExec>("grun") {
+    classpath = sourceSets.main.get().runtimeClasspath
+    description = "grun -gui"
+    group = antlrTasksGroup
+
+    mainClass.set("org.antlr.v4.gui.TestRig")
+
+    args = listOf(
+        "${tetradPackage}.Tetrad",
+        "init",
+        "-gui"
+    )
+
+    doFirst {
+        val scanner = Scanner(System.`in`)
+        val input = scanner.nextLine()
+
+        standardInput = ByteArrayInputStream(input.encodeToByteArray())
+        scanner.close()
     }
 }
 
